c80c3f530ad453bc003209b11ed75f5e8dee0c04
Mehmet KİTİŞ <103045822+mehmetkitis@users.noreply.github.com>
2024-12-22 01:27:32 +0300

    v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        /*Person person = new Person("John", "Doe", 20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("LastName: " + person.getLastName());
+
+        System.out.println("Age: " + person.getAge());*/
+
+        /*Person secondPerson = new Person("mehmet","kitiş",29,40000,
+                false,new String[]{"watching","reading"});
+
+        System.out.println("Firstname: " + secondPerson.getFirstName());
+
+        System.out.println("LastName: " + secondPerson.getLastName());
+
+        System.out.println("Age: " + secondPerson.getAge());
+
+        System.out.println("Hobbies: " + Arrays.toString(secondPerson.getHobbies()));*/
+
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    double salary;
+    boolean isMarried;
+    String[] hobbies;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age, double salary, boolean isMarried, String[] hobbies) {
+        this(firstName,lastName,age);
+        this.salary = salary;
+        this.isMarried = isMarried;
+        this.hobbies = hobbies;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public boolean isTeen(){
+        return this.age>=13 && this.age<=19;
+    }
 }
